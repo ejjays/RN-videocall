c7db394137830dbcff5f165447ce7d9f905b41d8
Ejjay <ejalloso021@gmail.com>
2025-06-20 18:49:14 +0000

    ...

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
-console.log("--- metro.config.js (v5 - Goldilocks) is being loaded! ---");
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// This is the key change. We are NO LONGER blocking the whole node_modules folder.
-// We are ONLY blocking the specific junk folders that cause the watcher to crash.
-config.resolver.blockList = [
-  /.*\/__tests__\/.*/,
-  /.*\/test\/.*/,
-  // This is a more specific rule for the firebase test files that were crashing before
-  /.*\/@firebase\/firestore\/dist\/firestore\/test\/.*/,
+// Add the project's node_modules to the resolver
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
 ];
 
-// We still need these to help the watcher behave in Termux
-config.watchFolders = [__dirname];
-config.watcher.healthCheck.enabled = false;
+// Explicitly tell Metro how to resolve the missing package
+config.resolver.extraNodeModules = {
+  '@stream-io/react-native-webrtc': path.resolve(__dirname, 'node_modules/@stream-io/react-native-webrtc'),
+};
 
 module.exports = config;
